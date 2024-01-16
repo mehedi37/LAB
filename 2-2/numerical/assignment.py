c0356ec8081139e6de9fc0cadfc492b687aec057
mehedi37 <mjmehedihasanoo7@gmail.com>
2024-01-16 07:26:56 +0600

    update

--- 2-2/numerical/assignment.py
+++ 2-2/numerical/assignment.py
@@ -1,78 +1,111 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sympy import symbols, diff, simplify, together, Poly
 from scipy.optimize import curve_fit
 import math
 
-def NewtonInterpolation(years, population, target_year):        # forward method
+# This function will generate the difference table for the given data.
+def difference_table(population):
     delta = []
     delta.append(population)
     for i in range(len(population)-1):
         delta.append([])
         for j in range(len(population)-i-1):
             delta[i+1].append(delta[i][j+1] - delta[i][j])
-    # print the delta table
-    # print("Delta table:")
-    # for i in range(len(delta)):
-    #     print(delta[i])
+    print("Delta table:")
+    print(delta)
+    return delta
 
+
+# This function finds the polynomial of the given data using Newton's Forward Interpolation method.
+def find_the_polynomial(delta, years, population):
+    '''
+        Yn(X) = Y0 + p * delta1 + p * (p-1) * delta2 / 2! + p * (p-1) * (p-2) * delta3 / 3! + ...
+    '''
+
+    x = symbols('x')
+    p = (x - years[0]) / (years[1] - years[0])
+    eqn = population[0]
+
+    static_p = p
+    fact = 1
+    for i in range(1, len(delta)):
+        fact *= i
+        term = p * delta[i][0] / fact
+        eqn += term
+        p *= (static_p - i)
+
+    # Simplify the expression
+    eqn = simplify(eqn)
+    derivative = diff(eqn, x)
+    print(f"\n\nEquation: {eqn}\n\nDerivative: {derivative}")
+
+    return [eqn, derivative]
+
+
+# It solves the equation for the given value of x.
+def func(eqn, x):
+    solution = round(eqn.subs('x', x), 6)       # the more the decimal places, the more accurate the result
+    # print(f"Solution for x = {x}: {solution}")
+    return solution
+
+def curveGeneralEqn(x, *coefficients):
+    # Assuming coefficients are in the form [a0, a1, a2, ...]
+    return sum(a * x**i for i, a in enumerate(coefficients))
+
+
+def NewtonInterpolation(eqn, target_year):
     '''
     we know,
         X = X0 + p * h
         p = (x - X0) / h
+
+    So,
+        Yn(X) = Y0 + p * delta1 + p * (p-1) * delta2 / 2! + p * (p-1) * (p-2) * delta3 / 3! + ...
     '''
-    p = (target_year - years[0]) / (years[1] - years[0])
+    return func(eqn, target_year)
 
-    '''
-    Yn(X) = Y0 + p * delta1 + p * (p-1) * delta2 / 2! + p * (p-1) * (p-2) * delta3 / 3! + ...
-    '''
 
-    target_population = population[0]
-    static_p = p
-    for i in range(1, len(delta)):
-        target_population += p * delta[i][0] / math.factorial(i)    # forward method, so delta[i][0]
-        p *= (static_p-i)
 
-    return target_population
-
-def raphson_func(x):
-    function = x**3 - 2*x - 5
-    derivative = 3*x**2 - 2
-    return [function, derivative]
-
-def newton_raphson(population, accuracy = 0.0001):
-    Xn = population - raphson_func(population)[0] / raphson_func(population)[1]
-    while abs(Xn - population) > accuracy:
-        population = Xn
-        if (raphson_func(population)[1] == 0):
-            print("Error! - derivative zero for x = ", population)
+#  I will be given target population and I have to find the year
+def newton_raphson(eqn, derivative, pop, initial_guess, accuracy = 0.001, max_iter=100):
+    x = initial_guess
+    Xn = x - (func(eqn, x) / func(derivative, x))
+    while abs(Xn - x) > accuracy and max_iter > 0:
+        max_iter = max_iter-1
+        x = Xn
+        if (func(derivative, x) == 0):
+            print("Error! - derivative zero for x = ", x)
             return None
-        Xn = population - raphson_func(population)[0] / raphson_func(population)[1]
+        Xn = x - (func(eqn, x) / func(derivative, x))
     return Xn
 
 
 
-# def func(x, m, c):
-#     return m*x + c
+def curve_fitting(eqn, years, population):
+    '''
+    Our target is to take the N.Interpolation General Eqn and make a new eqn with unknown coefficients.
+    so,
+        from 1250*x**(5/3) - 4170000*x**4 + 16693341250*x**3 - 33413380800000*x**2 + 100320284607985000*x/3 - 13386729871852880000
+        we get a0 + a1*x + a2*x**2 + a3*x**3 + a4*x**4 + a5*x**5
+        then we will solve this equation for the given values of x and y.
+        Find the values of a0, a1, a2, a3, a4, a5.
+        Then we will substitute the values of a0, a1, a2, a3, a4, a5 in the general equation.
+        Then we will get the curve fitting equation.
+        We will use this equation to find the population for the next 10 years.
+    '''
+    x = symbols('x')
+    # Convert expression to a rational function
+    rational_expression = together(eqn)
+    # taking all the coefficients of the equation
+    coefficients = Poly(rational_expression, x).all_coeffs()
+    # Determine the order of the equation
+    order = len(coefficients) - 1
+    initial_guess = np.ones(order)
+    params, _ = curve_fit(curveGeneralEqn, years, population, p0=initial_guess)
+    return params
 
-# def curve_fitting(years, population, target_year):      # straight line method
-#     popt, _ = curve_fit(func, years, population)
-#     predicted_population = func(target_year, *popt)
-#     return predicted_population
-
-
-
-def curve_fitting(years, population, target_year):      # straight line method
-    n = len(years)
-    sum_x = sum(years)
-    sum_y = sum(population)
-    sum_x_squared = sum([i**2 for i in years])
-    sum_xy = sum([years[i]*population[i] for i in range(n)])
-
-    m = (n*sum_xy - sum_x*sum_y) / (n*sum_x_squared - sum_x**2)
-    c = (sum_y - m*sum_x) / n
-
-    predicted_population = m*target_year + c
-    return predicted_population
 
 if __name__ == "__main__":
     data = pd.read_csv('c:\\Users\\mjmeh\\Documents\\Github\\LAB\\2-2\\numerical\\data.csv')
@@ -85,22 +118,29 @@ if __name__ == "__main__":
     # Next 10 years for prediction
     target_year_values = [last_year + i for i in range(1, 11)]
 
+    delta = difference_table(population)
+    eqn, derivative = find_the_polynomial(delta, years, population)
+    # print(eqn - symbols(str(134480000)))      # Population(t) - target_population = 0
+
+    # curve fitting parameters
+    curveParams = curve_fitting(eqn, years, population)
+
     # Calculate populations for Newton Interpolation and Curve Fitting
-    target_population_newton = [NewtonInterpolation(years, population, year) for year in target_year_values]
-    target_population_newton_raphson = [newton_raphson(pop) for pop in target_population_newton]
-    target_population_curve_fit = [curve_fitting(years, population, year) for year in target_year_values]
+    target_population_newton = [NewtonInterpolation(eqn, year) for year in target_year_values]
+    target_years_newton_raphson = [newton_raphson(eqn - pop, derivative, pop, last_year + 1) for pop in target_population_newton]
+    target_population_curve_fit = [curveGeneralEqn(year, *curveParams) for year in target_year_values]
 
     print("\nNewton Interpolation: ")
     for year, pop in zip(target_year_values, target_population_newton):     # makes a dictionary
-        print("Population in", year, "(N.InterPol): ", pop)
+        print("Population in", year, " : ", pop)
 
     print("\nNewton Raphson: ")
-    for year, pop in zip(target_year_values, target_population_newton_raphson):
-        print("Population in", year, "(N.Raphson): ", pop)
+    for year, pop in zip(target_years_newton_raphson, target_population_newton):
+        print(f"In {year} ({round(year)}) Year Population: {pop}")
 
     print("\nCurve Fitting: ")
     for year, pop in zip(target_year_values, target_population_curve_fit):
-        print("Population in", year, "(CurveFit): ", pop)
+        print("Population in", year, " : ", pop)
 
 
     plt.rcParams.update({
@@ -109,7 +149,7 @@ if __name__ == "__main__":
     })
 
     plt.plot(target_year_values, target_population_newton, 'ro-', label='N.Interpolation')
-    plt.plot(target_year_values, target_population_newton_raphson, 'go-', label='N.Raphson')
+    plt.plot(target_years_newton_raphson, target_population_newton, 'go-', label='N.Raphson')
     plt.plot(target_year_values, target_population_curve_fit, 'bo-', label='Curve Fitting')
 
     plt.xlabel('Year')
--- 2-2/numerical/selfStudy.py
+++ 2-2/numerical/selfStudy.py
@@ -1,10 +1,37 @@
-list = [""]
-st = ""
-for i in list:
-    st += (i if i != "" else "&nbsp") + ",|,"
-st = st[:-3]
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
-print(st)
-l = len(st)
-st = st.replace("&nbsp", "")
-print([] if l == 0 else st.split(",|,"))
+def curveGeneralEqn(x, *coefficients):
+    # Assuming coefficients are in the form [a0, a1, a2, ...]
+    return sum(a * x**i for i, a in enumerate(coefficients))
+
+# Given x and y values
+x_values = np.array([1, 2, 3, 4, 5])  # Replace with your actual x values
+y_values = np.array([100, 150, 200, 250, 300])  # Replace with your actual y values
+
+# Use a lambda function to dynamically estimate the size of parameters
+params_function = lambda x: np.ones(len(x))
+
+# Use curve_fit to fit the curve with a dynamically estimated initial guess
+params, covariance = curve_fit(curveGeneralEqn, x_values, y_values, p0=params_function(x_values))
+
+# Print the fitted parameters
+print("Fitted Parameters:")
+for i, param in enumerate(params):
+    print(f"a{i} = {param}")
+
+# Generate x values for the next 10 years
+next_10_years = np.arange(x_values[-1] + 1, x_values[-1] + 11)
+
+# Predict y values for the next 10 years using the fitted model
+predicted_population = curveGeneralEqn(next_10_years, *params)
+
+# Plot the original data and the extended curve
+plt.scatter(x_values, y_values, label='Original Data')
+plt.plot(x_values, curveGeneralEqn(x_values, *params), label='Fitted Curve', color='red')
+plt.plot(next_10_years, predicted_population, label='Extended Curve', linestyle='--', color='green')
+plt.xlabel('Year')
+plt.ylabel('Population')
+plt.legend()
+plt.show()
